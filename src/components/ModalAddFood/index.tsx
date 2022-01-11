7bbb785baa385f8eef24ac916ce0765cf09d8b02
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-11 14:09:26 -0300

    Alterado componente ModalAddFood para TS e Function Components

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,50 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+
+interface ModalAddFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (data: FoodData) => void;
+}
+
+interface FoodData {
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+}
+
+const ModalAddFood = ({ isOpen, setIsOpen, handleAddFood } : ModalAddFoodProps) => {
+  const formRef = useRef(null);
+
+  const handleSubmit = async (data : FoodData) => {
+    handleAddFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" icon={null} />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" icon={null}/>
+        <Input name="price" placeholder="Ex: 19.90" icon={null}/>
+
+        <Input name="description" placeholder="Descrição" icon={null}/>
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalAddFood;
